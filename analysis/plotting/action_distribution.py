fb6fb3cfb461604428fc5fc65f75363a37e28b0a
Kate Avery <kteavery@users.noreply.github.com>
2021-09-12 21:34:55 -0400

    normalize plots, fix bounds

--- analysis/plotting/action_distribution.py
+++ analysis/plotting/action_distribution.py
@@ -38,23 +38,45 @@ def variance_max_actions(dists):
     return np.std(np.argmax(dists, axis=1))
 
 
-def plot_js_divergence_matrix(data, title):
+def plot_js_divergence_matrix(data, vanilla, title, normalize=True):
     agent = data[:, 0]
     state = data[:, 1]  # 0 indexed
     intv = data[:, 2]  # 0 indexed
-    mat = np.zeros((np.max(state).astype(int) + 1, np.max(intv).astype(int) + 1))
+    van_state = vanilla[:, 1]  # 0 indexed
+    intv_mat = np.zeros((np.max(state).astype(int) + 1, np.max(intv).astype(int) + 1))
+    van_mat = np.zeros((np.max(state).astype(int) + 1, 1))
+    mat = np.concatenate((van_mat, intv_mat), axis=1)
 
     for s in np.unique(state).astype(int):
+        van_s = (vanilla[:, 1] == s)
+        van_wh = np.where(van_s)[0]
+        van_agent = vanilla[van_wh, 3:]
+        assert len(vanilla[van_wh, 0]) == len(np.unique(vanilla[van_wh, 0]))
+        norm_van = js_divergence(van_agent) / np.log2(10)
+        mat[s, 0] = norm_van
+        if normalize == True:
+            mat[s, 0] = 0.0
+        avg = 0
+
         for i in np.unique(intv).astype(int):
             si = (data[:, 1] == s) * (data[:, 2] == i)
             wh = np.where(si)[0]
             agents = data[wh, 3:]
             assert len(data[wh, 0]) == len(np.unique(data[wh, 0]))
-            mat[s, i] = js_divergence(agents)
+            mat[s, i+1] = js_divergence(agents)/np.log2(10)
+            if normalize == True:
+                mat[s, i+1] -= norm_van
+            avg += mat[s, i+1]
+
+        avg /= len(np.unique(intv).astype(int))
 
     im = plt.matshow(mat, interpolation="none")
     cbar = plt.colorbar(im)
     cbar.set_label("JS Divergence of Action Distributions")
+    if normalize == True:
+        plt.clim(-1.0, 1.0)
+    else:
+        plt.clim(0,1.0)
     ax = plt.gca()
     ax.tick_params(axis="x", top=False, bottom=True, labelbottom=True, labeltop=False)
     plt.title(title)
@@ -63,6 +85,20 @@ def plot_js_divergence_matrix(data, title):
 
     os.makedirs("storage/plots/jsdivmat", exist_ok=True)
     plt.savefig(f"storage/plots/jsdivmat/{title}.png")
+
+
+def plot_individual_distributions(agents, mat):
+    im = plt.matshow(agents, interpolation="none", aspect="auto")
+    cbar = plt.colorbar(im)
+    plt.clim(0.0,1.0)
+    ax = plt.gca()
+    ax.tick_params(axis="x", top=False, bottom=True, labelbottom=True, labeltop=False)
+    plt.title(mat)
+    plt.xlabel('actions')
+    plt.ylabel('runs')
+    os.makedirs(f"storage/plots/jsdivmat/distributions - {title}", exist_ok=True)
+    plt.savefig(f"storage/plots/jsdivmat/distributions - {title}/{i}_{s}.png")
+    plt.close()
 
 
 def plot_max_action_divergence_matrix(data, title):
@@ -80,9 +116,12 @@ def plot_max_action_divergence_matrix(data, title):
             assert len(data[wh, 0]) == len(np.unique(data[wh, 0]))
             mat[s, i] = variance_max_actions(agents)
 
-    im = plt.matshow(mat, interpolation="none")
+    norm_mat = mat/np.log2(10)
+
+    im = plt.matshow(norm_mat, interpolation="none")
     cbar = plt.colorbar(im)
     cbar.set_label("Variance of Max Action")
+    plt.clim(0.0,1.0)
     ax = plt.gca()
     ax.tick_params(axis="x", top=False, bottom=True, labelbottom=True, labeltop=False)
     plt.title(title)
@@ -97,11 +136,18 @@ if __name__ == "__main__":
     n_agents = 10
     nstates = 30
     horizon = 100
-    for fam in ["a2c", "dqn"]:
+    for fam in ["a2c", "dqn", "ddqn", "rainbow", "c51"]:
         dir = f"storage/results/intervention_action_dists/{fam}/{n_agents}_agents/{nstates}_states/t{horizon}_horizon"
         plot_js_divergence_matrix(
-            np.loadtxt(dir + "/88_interventions.txt"),
-            f"JS Divergence of Actions for {fam}, {n_agents} Agents, t={horizon} horizon",
+            np.loadtxt(dir + "/88_interventions.txt"), 
+            np.loadtxt(dir + "/vanilla.txt"),
+            f"Normalized JS Divergence of Actions for {fam}, {n_agents} Agents, t={horizon} horizon",
+        )
+        plot_js_divergence_matrix(
+            np.loadtxt(dir + "/88_interventions.txt"), 
+            np.loadtxt(dir + "/vanilla.txt"),
+            f"Unnormalized JS Divergence of Actions for {fam}, {n_agents} Agents, t={horizon} horizon",
+            normalize=False
         )
         plot_max_action_divergence_matrix(
             np.loadtxt(dir + "/88_interventions.txt"),
