4177f40d34eca2e78a0e2ec8caa5e5df6c06ba6a
Jack Kenney <jack@kenney.dev>
2022-01-07 23:12:07 -0500

    add script to quickly make all the megaplots

---
+++ analysis/plotting/make_megaplots.sh
@@ -0,0 +1,6 @@
+python -m analysis.plotting.sampled_jsdiv Amidar &
+python -m analysis.plotting.sampled_jsdiv Amidar norm &
+python -m analysis.plotting.sampled_jsdiv Breakout &
+python -m analysis.plotting.sampled_jsdiv Breakout norm &
+python -m analysis.plotting.sampled_jsdiv SpaceInvaders &
+python -m analysis.plotting.sampled_jsdiv SpaceInvaders norm &
