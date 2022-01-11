fix normalization bug

analysis/plotting/tables.py
@@ -2,6 +2,7 @@ import numpy as np
 from analysis.src.js_divergence import get_js_divergence_matrix
 from envs.wrappers.paths import get_num_interventions
 
+import matplotlib.pyplot as plt
 
 from runners.src.run_intervention_eval import (
     supported_environments,
@@ -37,9 +38,11 @@ def print_values_table(
     C = len(checkpoints)
     table = np.zeros((F, C, 3))
 
+    print("JS DIV")
     print()
-    print("\\begin{tabular}{|l|l|c|c|c|}\\hline")
-    print("Family & Checkpoint & Original & Intervened & Normalized \\\\\\hline")
+    print("\\begin{tabular}{@{}lllll@{}}")
+    print("\\toprule")
+    print("Family & Frames & Original & Intervened & Normalized \\\\\\midrule")
 
     for f, fam in enumerate(families):
         for c, check in enumerate(checkpoints):
@@ -51,9 +54,12 @@ def print_values_table(
             if check == "":
                 check = 10000000
 
+            order = int(np.log10(check))
+
             print(
-                f"{fam} & {'{:.0e}'.format(check)} & {round(v, 4)} & {round(u, 4)} & {round(n, 4)} \\\\\\hline"
+                f"{fam} & {f'{int(check / 10**order)}e{order}'} & {v:.3f} & {u:.3f} & {n:.3f} \\\\"
             )
+    print("\\bottomrule")
     print("\\end{tabular}")
 
     return table
@@ -65,6 +71,7 @@ def makeTables(nAgents=11, nStates=30):
     normalized_dict = {}
 
     for env in supported_environments:
+        print(env)
         nintv = get_num_interventions(env)
 
         vanilla_dict[env] = {}
@@ -85,12 +92,19 @@ def makeTables(nAgents=11, nStates=30):
                 mat, nmat, van_mat, intv_mat, n_intv_mat = get_js_divergence_matrix(
                     data, vdata
                 )
+                # van_mat = 1 - van_mat
+                # intv_mat = 1 - intv_mat
+                # n_intv_mat = (2 - (n_intv_mat + 1)) - 1
+                # if fam == "dqn":
+                #     plt.hist(intv_mat)
+                #     plt.title(f"{env}, {fam}, {check} normalized")
+                #     plt.show()
 
                 vanilla_dict[env][fam][check] = van_mat.mean()
                 normalized_dict[env][fam][check] = n_intv_mat.mean()
                 unnormalized_dict[env][fam][check] = intv_mat.mean()
 
-        print_image_name_table(model_names, env)
+        # print_image_name_table(model_names, env)
         print_values_table(
             env,
             model_names,

analysis/src/js_divergence.py
@@ -40,8 +40,8 @@ def get_js_divergence_matrix(data, vanilla):
     mat[:, 1:] = intv_mat
 
     # normalized
-    n_intv_mat = intv_mat / van_mat.reshape(-1, 1) - 1
-    n_van_mat = van_mat / van_mat - 1
+    n_intv_mat = intv_mat - van_mat.reshape(-1, 1)
+    n_van_mat = 0
     nmat = np.zeros((nstates, nintv + 1))
     nmat[:, 0] = n_van_mat
     nmat[:, 1:] = n_intv_mat