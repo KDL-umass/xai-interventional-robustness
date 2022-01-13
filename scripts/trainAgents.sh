f41dd76f8f79e3a9e2ad0634c732e401df75ade3
Jack Kenney <jack@kenney.dev>
2022-01-13 11:35:52 -0500

    update scripts

--- scripts/trainAgents.sh
+++ scripts/trainAgents.sh
@@ -6,6 +6,9 @@ for env in Amidar Breakout SpaceInvaders
 do
     for fam in a2c c51 dqn ddqn ppo rainbow vqn vsarsa
     do
-        python -m runners.src.run_experiment --env $env --family $fam;
+        for n in {0..10} # create 11 agents
+        do
+            python -m runners.src.run_experiment --env $env --family $fam;
+        done
     done
 done
